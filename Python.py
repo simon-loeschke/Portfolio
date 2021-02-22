aca0f88d4cc5904e194fd9ebf95d2fca802f561a
Der Cybercoach <simon-loeschke@posteo.de>
2021-02-23 00:07:23 +0100

    kleines Python programm

--- Python.py
+++ Python.py
@@ -1,3 +1,19 @@
-!#/bin/python
+#!/bin/python
 
-#Das ist ein Python-Programm
+#Das ist ein kleines terminal-Programm, das Ihr Alter in tagen auswirft.
+
+name = input("Wie ist Ihr Name? \n")
+
+print("\n \n Hallo " + name)
+
+print("\n" +  name + ", bitte geben Sie Ihr Alter an: \n")
+
+alter = int(input())  
+
+days_calc = alter * 365
+
+days = str(days_calc)
+
+print("\n \n Sie sind " + days + " Tage alt.")
+
+
